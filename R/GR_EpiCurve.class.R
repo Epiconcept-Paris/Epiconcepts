02b35cba83f0c99a71155b7b24a7004db45305f5
jpKDO <kdo@zpmag.com>
2017-07-05 15:05:58 +0200

    Scaling Y with pretty scales

--- R/GR_EpiCurve.class.R
+++ R/GR_EpiCurve.class.R
@@ -105,30 +105,22 @@ ec.epiCurve <- function(x,
       mutate(Date = Mois)
   }
   
+  # Init pseudo variables (in AES) for packaging
+  Date <- Freq <- Day <- Mois <- Cut <- NULL
+  
   P_ <- ggplot(arrange(DF, Cut), aes(x=Date, y=Freq, fill=factor(Cut))) 
   P_ <- P_ +  geom_bar(stat='identity', width=1);
   
   P_ <- P_ + scale_fill_manual(values = .color, labels=.cutorder,
                                breaks=levels(DF$Cut), limits=levels(DF$Cut),
-                               guide = guide_legend(reverse = TRUE))
-  # Compute y ticks
-  # -----------------------------------------------------------------
-  if (MaxValue > 2) {
-    P_ <- P_ + scale_y_continuous(expand = c(0,0))
-  } else {
-    P_ <- P_ + scale_y_continuous(expand = c(0,0), breaks = c(0,seq(1,MaxValue)))
-  }
-  
-  # Draw hlines and vlines
-  # -----------------------------------------------------------------
-  P_ <- P_ + geom_hline(yintercept=seq(1, MaxValue, by=1), colour="white", size=0.3)
-  
+                               guide = guide_legend(reverse = TRUE)) +
+  scale_y_continuous(breaks= pretty_breaks(), expand = c(0,0)) +
+    geom_hline(yintercept=seq(1, MaxValue, by=1), colour="white", size=0.3)
   if (nrow(DF) > 1) {
     P_ <- P_ + geom_vline(xintercept = seq(1.5, nrow(DF), by=1), colour="white", size=0.3)
   }
-  
   note <- gsub('(.{1,90})(\\s|$)', '\\1\n', note)
-  P_ <- P_ + xlab(paste(xlabel, note, sep="\n\n")) + 
+  P_ <- P_ + xlab(paste(xlabel, note, sep="\n\n")) +
     ylab(ylabel) +
     labs(title = title, fill = "") +
     coord_fixed(ratio=1) +
@@ -137,6 +129,34 @@ ec.epiCurve <- function(x,
     theme(axis.text.x = element_text(angle=90))+
     theme(axis.line.x = element_line(colour="black", linetype="solid", size = 0.5),
           axis.line.y = element_line(colour="black", linetype="solid", size = 0.5))
-    P_
+  P_
+  
+  # # Compute y ticks
+  # # -----------------------------------------------------------------
+  # if (MaxValue > 2) {
+  #   P_ <- P_ + scale_y_continuous(expand = c(0,0))
+  # } else {
+  #   P_ <- P_ + scale_y_continuous(expand = c(0,0), breaks = c(0,seq(1,MaxValue)))
+  # }
+  # 
+  # # Draw hlines and vlines
+  # # -----------------------------------------------------------------
+  # P_ <- P_ + geom_hline(yintercept=seq(1, MaxValue, by=1), colour="white", size=0.3)
+  # 
+  # if (nrow(DF) > 1) {
+  #   P_ <- P_ + geom_vline(xintercept = seq(1.5, nrow(DF), by=1), colour="white", size=0.3)
+  # }
+  # 
+  # note <- gsub('(.{1,90})(\\s|$)', '\\1\n', note)
+  # P_ <- P_ + xlab(paste(xlabel, note, sep="\n\n")) + 
+  #   ylab(ylabel) +
+  #   labs(title = title, fill = "") +
+  #   coord_fixed(ratio=1) +
+  #   theme_bw() +
+  #   theme(panel.border = element_blank()) +
+  #   theme(axis.text.x = element_text(angle=90))+
+  #   theme(axis.line.x = element_line(colour="black", linetype="solid", size = 0.5),
+  #         axis.line.y = element_line(colour="black", linetype="solid", size = 0.5))
+  #   P_
 }
 
